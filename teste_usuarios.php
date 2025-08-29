5315fe5de536556696d8f02161cc728cd3adcef5
Ian Derick <ian.derick@outlook.com>
2025-08-28 21:30:47 -0300

    Retirando arquivo teste

--- teste_usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "config/conexaodb.php";
-
-$stmt = $pdo->query("SELECT * FROM usuarios");
-$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo "<pre>";
-print_r($usuarios);
-echo "</pre>";
