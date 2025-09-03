b2d44728eaecc49c1c31720a5440764c3cd9402b
IanDerick <ian.derick@outlook.com>
2025-09-02 22:55:15 -0300

    Corrigindo include e fazendo conexão //casa

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
         </div>
     </div>
 
-    <?php include 'includes/modal_reset_senha.php'; ?>
+    <?php include 'includes/models/modal_reset_senha.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/script.js"></script>
