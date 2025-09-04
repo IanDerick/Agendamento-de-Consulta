488ac1e42d2ac6691f26254aaaa8d6c75e6e8c3b
Ian Derick <ian.derick@outlook.com>
2025-09-04 20:23:31 -0300

    Função para deletar doutor

---
+++ actions/exclui_doutor.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    require "../config/conexaodb.php";
+    
+    $id = $_GET["coddoutor"] ?? null;
+    if (!$id) {
+        $_SESSION['error'] = "ID do doutor não informado!";
+        header("Location: ../pages/novo_paciente.php");
+        exit();
+    }
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $stmt = $pdo->prepare("DELETE FROM DOUTOR WHERE CODDOUTOR = ?");
+        $stmt->execute([$id]);
+        $_SESSION['success'] = "Doutor deletado com sucesso!";
+    } catch (PDOException $e) {
+        $_SESSION['error'] = "Erro ao excluir no banco: " . $e->getMessage();
+    }
+    header("Location: ../pages/novo_doutor.php");
+    exit();
+?>
