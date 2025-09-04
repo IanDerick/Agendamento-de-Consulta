fe7868d6d029148dc2c5707ed2d96c94e477213a
Ian Derick <ian.derick@outlook.com>
2025-09-04 20:30:08 -0300

    Fazendo modal de editar doutor

---
+++ includes/models/modal_edita_doutor.php
@@ -0,0 +1,44 @@
+<div class="modal fade" id="modalEditaDoutor" tabindex="-1" aria-labelledby="modalEditaDoutorLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalEditaDoutorLabel">Novo Paciente</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="formEditaDoutor" action="../actions/atualiza_doutor.php" method="post">
+                    <div class="row">
+                        <div class="col-md-20">
+                            <div class="d-flex justify-content-center">
+                                <div class="mb-3 text-center">
+                                    <i class="bi bi-person-circle display-1 text-secondary"></i>
+                                    <small class="d-block mt-2 " id="previewNome"></small>
+                                </div>
+                            </div>
+                            <input type="hidden" id="coddoutor" name="coddoutor">
+                            <div class="mb-3">
+                                <label for="nome" class="form-label">Nome:</label>
+                                <input type="text" class="form-control" id="nome" name="nome" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">E-mail:</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="radio" class="form-check-input" id="ativo" name="status" value="1">
+                                <label class="form-check-label" for="ativo">Ativo</label>
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="radio" class="form-check-input" id="inativo" name="status" value="0">
+                                <label class="form-check-label" for="inativo">Inativo</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" form="formEditaDoutor" class="btn btn-green">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- pages/novo_doutor.php
+++ pages/novo_doutor.php
@@ -73,6 +73,7 @@
     </div>
 </div>
 <?php include '../includes/models/modal_novo_doutor.php'; ?>
+<?php include '../includes/models/modal_edita_doutor.php';?>
 <script src="../assets/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
